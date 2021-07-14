b3342a06982b2260dc4bbaddef586f37fd1f64c6
dustingottlieb <dustingottlieb@gmail.com>
2021-07-13 21:57:09 -0500

    edited resume and resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,6 +4,16 @@ import './Resume.css'
 export function Resume() {
     return (
         <div class="Resume">
+        <h5>Skills</h5>
+               <ul>
+               <li>JavaScript</li>
+               <li>Node</li>
+            <li>MongoDB</li>
+            <li>SQL</li>
+            <li>Sequilize</li>
+            <li>HTML5</li>
+            <li>CSS</li>
+        </ul>
         <a href="/DustinGottlieb-Resume.pdf" id="resume" download><h5>My Resume</h5></a></div>
     )
 }
