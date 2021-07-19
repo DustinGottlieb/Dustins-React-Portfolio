bf8704b8d66c300359288fc93879ca1f268ac48b
dustingottlieb <dustingottlieb@gmail.com>
2021-07-18 20:14:48 -0500

    added mobile responsiveness

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,16 +5,17 @@ export function Resume() {
     return (
         <div class="Resume">
         <h5>Skills</h5>
-               <ul>
-               <li>JavaScript</li>
-               <li>Node</li>
+        <div class="skills">
+        <ul>
+            <li>JavaScript</li>
+            <li>Node</li>
             <li>MongoDB</li>
             <li>SQL</li>
             <li>Sequilize</li>
             <li>HTML5</li>
             <li>CSS</li>
         </ul>
-        <a href="/DustinGottlieb-Resume.pdf" id="resume" download><h5>My Resume</h5></a></div>
+        <a href="/DustinGottlieb-Resume.pdf" id="resume" download><h5>My Resume</h5></a></div></div>
     )
 }
 
